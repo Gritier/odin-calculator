bdda59cb51b1495856a99922981e14573d34b98c
Gritier <28902214+Gritier@users.noreply.github.com>
2024-05-29 00:37:44 +0200

    Complete the project with all its functionality

--- main.js
+++ main.js
@@ -3,19 +3,32 @@ const previousText = document.querySelector('#display>p');
 const displayText = document.querySelector("#display>h3");
 const numberButtons = document.querySelectorAll('button.number');
 const operatorButtons = document.querySelectorAll('button.operator');
-const clearButton = document.querySelector("#clear");
+const clearButton = document.querySelector("#clear-button");
 const equalButton = document.querySelector("#equal");
+
+let operationFlag = false;
 
 for (const button of Array.from(numberButtons)){
     button.addEventListener("click", () => {
-        displayText.textContent += button.textContent;
+        if (operationFlag === true) {
+            operationFlag = false;
+            displayText.textContent = '';
+        }
+        if (displayText.textContent === '' && button.textContent === '.'){
+            displayText.textContent += '0.'
+        }else if (displayText.textContent.includes('.') && button.textContent === '.') {
+        } else {
+            displayText.textContent += button.textContent;
+        }
     })
 }
 
 for (const button of Array.from(operatorButtons)){
     button.addEventListener('click', () => {
-        if (displayText.textContent !== '') {
-            previousText.textContent += ` ${displayText.textContent} ${button.textContent}`;
+        if (displayText.textContent !== '' && previousText.textContent !== ''){
+            executeOperation(displayText.textContent, previousText.textContent);
+        }else if (displayText.textContent !== '') {
+            previousText.textContent += `${displayText.textContent} ${button.textContent}`;
             displayText.textContent = '';
         } else if (displayText.textContent === '' && previousText.textContent !== '') {
             previousText.textContent = previousText.textContent.slice(0,-1)+button.textContent;
@@ -23,6 +36,20 @@ for (const button of Array.from(operatorButtons)){
 
     })
 }
+
+clearButton.addEventListener('click', () => {
+    if (displayText.textContent === ''){
+        previousText.textContent = '';
+    } else {
+        displayText.textContent = '';
+    }
+})
+
+equalButton.addEventListener('click', () => {
+    if (displayText.textContent !== '' && previousText.textContent !== ''){
+        executeOperation(displayText.textContent, previousText.textContent)
+    }
+})
 
 function add(firstNumber, secondNumber){
     return firstNumber+secondNumber;
@@ -42,12 +69,59 @@ function divide(dividend, divisor){
 
 function operate(firstNumber, secondNumber, operand){
     if (operand === '+'){
-        display.textContent = toString(add(firstNumber, secondNumber));
+        return add(firstNumber, secondNumber);
     } else if (operand === '-'){
-        display.textContent = toString(subtract(firstNumber, secondNumber));
+        return subtract(secondNumber, firstNumber);
     } else if (operand === '*'){
-        display.textContent = toString(multiply(firstNumber, secondNumber));
+        return multiply(firstNumber, secondNumber);
     } else if (operand === '/') {
-        display.textContent = toString(divide(firstNumber, secondNumber));
+        if (firstNumber === 0) {
+            return `We can't divide by 0`;
+        }
+        return divide(secondNumber, firstNumber);
     }
 }
+
+function adaptiveParse(numberString){
+    if (numberString.includes('.')){
+        return parseFloat(numberString);
+    } else {
+        return parseInt(numberString);
+    }
+}
+
+function executeOperation(displayString, previousString){
+    operationFlag = true;
+    displayText.textContent = operate(adaptiveParse(displayString), adaptiveParse(previousString.split(" ")[0]), previousString.split(" ")[1]).toString();
+    previousText.textContent = '';
+}
+
+//Keyboard support
+document.addEventListener('keypress', (event) =>{
+console.log(event.key);
+        if (!isNaN(parseInt(event.key))){
+        if (operationFlag === true) {
+            operationFlag = false;
+            displayText.textContent = '';
+        }
+        displayText.textContent += event.key;
+        } else if (event.key === '+' ||event.key === '-' ||event.key === '*' ||event.key === '/'){
+            if (displayText.textContent !== '' && previousText.textContent !== ''){
+                executeOperation(displayText.textContent, previousText.textContent);
+            }else if (displayText.textContent !== '') {
+                previousText.textContent += `${displayText.textContent} ${event.key}`;
+                displayText.textContent = '';
+            } else if (displayText.textContent === '' && previousText.textContent !== '') {
+                previousText.textContent = previousText.textContent.slice(0,-1)+event.key;
+            }
+        } else if (event.key === '=' || event.key === 'Enter') {
+            if (displayText.textContent !== '' && previousText.textContent !== ''){
+                executeOperation(displayText.textContent, previousText.textContent)
+            }
+        } else if (event.key === '.') {
+            if (!displayText.textContent.includes('.')){
+                displayText.textContent += '.';
+            }
+        }
+    }
+)
